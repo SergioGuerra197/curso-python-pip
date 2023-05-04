2436c68398c61f1f0d83f68ad1c5ecf8edd01b55
Sergio Andres Guerra <guerrasergion@gmail.com>
2023-05-03 21:47:34 -0500

    Se agregaron links de herramientas utiles, y archivo readme y gitignore

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+#Herramientas 
+#https://www.toptal.com/developers/gitignore/
+#https://pandao.github.io/editor.md/en.html
+
 print("Mi primer programa Python del curso de PIP")
 print("Segundo commit")
 print("link configuracion llaves ssh de windows en WSL=> https://platzi.com/tutoriales/1557-git-github/7538-copiar-llaves-ssh-de-windows-a-wsl-2/")
