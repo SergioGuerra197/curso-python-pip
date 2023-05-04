cb7ca709f016c09d2b3fcb593a55503f16690aab
Sergio Andres Guerra <guerrasergion@gmail.com>
2023-05-03 21:37:12 -0500

    Agrego link solucion problema de llaves ssh

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 print("Mi primer programa Python del curso de PIP")
 print("Segundo commit")
+print("link configuracion llaves ssh de windows en WSL=> https://platzi.com/tutoriales/1557-git-github/7538-copiar-llaves-ssh-de-windows-a-wsl-2/")
